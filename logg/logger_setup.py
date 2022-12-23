c64e07db271e3ebf7966852075aafbe307aabafd
User <makaroffglobal@icloud.com>
2022-12-23 23:47:22 +0300

    Amended logging

--- logg/logger_setup.py
+++ logg/logger_setup.py
@@ -9,7 +9,6 @@ import yaml
 def setup_logging(config_logger):
 
     if not os.path.exists("logg2"):
-        os.makedirs("logg2")
 
-    with open(config_logger) as config_fin:
-        logging.config.dictConfig(yaml.safe_load(config_fin))
+        with open(config_logger) as config_fin:
+            logging.config.dictConfig(yaml.safe_load(config_fin))
